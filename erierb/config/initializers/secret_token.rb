0cf91846036a2a6dcf9d6a7f3cfa4a1964b34541
Paul Barrick <paul@imperativedesign.net>
2013-02-12 22:05:32 -0500

    initial ruby commit

---
+++ erierb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erierb::Application.config.secret_token = 'ead4cf992a990535e2a6a820093afc6e5bca510b27dff4b07aef766181c17caa4044fd2a835bf854e4172004ec9b15e8c46c384d8ae2c793e8b5336cf7a821e8'
